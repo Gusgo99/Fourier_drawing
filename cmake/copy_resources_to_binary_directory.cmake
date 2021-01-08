342c006334a9286b51e0d6d3f31d6cdd72a32a58
Gusgo99 <Gusgo99@users.noreply.github.com>
2021-01-07 21:23:53 -0300

    Changed spaces to tabs in cmake files

--- cmake/copy_resources_to_binary_directory.cmake
+++ cmake/copy_resources_to_binary_directory.cmake
@@ -1,9 +1,9 @@
 function(copy_resources_to_binary_directory)
-    # Copies resources files to the directory with where the executable goes
-    file(GLOB_RECURSE RESOURCES "${PROJECT_SOURCE_DIR}/resources/*")
-    file(
-        COPY
-        "${PROJECT_SOURCE_DIR}/resources/layout.xrc"
-        DESTINATION
-        "${CMAKE_BINARY_DIR}/resources")
+	# Copies resources files to the directory with where the executable goes
+	file(GLOB_RECURSE RESOURCES "${PROJECT_SOURCE_DIR}/resources/*")
+	file(
+		COPY
+		"${PROJECT_SOURCE_DIR}/resources/layout.xrc"
+		DESTINATION
+		"${CMAKE_BINARY_DIR}/resources")
 endfunction()
